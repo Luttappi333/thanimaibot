39bc795b8076153d7879838f7df4f8a85058ace8
Luttappi333 <101881271+Luttappi333@users.noreply.github.com>
2022-08-02 14:18:12 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -67,13 +67,13 @@ PM_START_TEXT = """
 ┏━━━━━•❃°•°❀°•°❃•━━━━┓
 ×  `{}` users, across `{}` chats..
 ┗━━━━━•❃°•°❀°•°❃•━━━━┛
-๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅs ʙ
+
 
 
 
 ๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅs ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs.
 
-➼ So What U Waiting For Add Me To Ur chat
+➼ So What U Waiting For Add Me To Ur chat.
 
 
 
